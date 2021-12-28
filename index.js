2fa8e95baf88265bf154168a431aab8e924a4e60
unknown <khuongduy354@gmail.com>
2021-12-28 11:30:31 +0700

    fix order black white

--- index.js
+++ index.js
@@ -2,7 +2,49 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 const Board_1 = require("./chess/Board");
 const Coordinates_1 = require("./chess/Coordinates");
-let newGame = new Board_1.ChessBoard();
-newGame.renderBoard();
-newGame.move(new Coordinates_1.MapCor(1, 3), new Coordinates_1.MapCor(2, 3));
-newGame.renderBoard();
+//chess algebraic notation to string
+let chessAGToCor = (ag) => {
+    if (!/[a-h][1-8]/.test(ag)) {
+        throw new Error("Enter valid move");
+    }
+    else {
+        var yCor = 0;
+        var xCor = 8 - parseInt(ag[1]);
+        switch (ag[0]) {
+            case "a":
+                yCor = 0;
+            case "b":
+                yCor = 1;
+            case "c":
+                yCor = 2;
+            case "d":
+                yCor = 3;
+            case "e":
+                yCor = 4;
+            case "f":
+                yCor = 5;
+            case "g":
+                yCor = 6;
+            case "h":
+                yCor = 7;
+        }
+        console.log(yCor);
+        return new Coordinates_1.MapCor(xCor, yCor);
+    }
+};
+class GameController {
+    constructor() {
+        this.game = new Board_1.ChessBoard();
+    }
+    move(from, to) {
+        this.game.move(chessAGToCor(from), chessAGToCor(to));
+    }
+    render() {
+        this.game.renderBoard();
+    }
+}
+// let game1 = new GameController();
+let game = new Board_1.ChessBoard();
+game.renderBoard();
+// game1.move("a2", "a3");
+// game1.render();
--- index.ts
+++ index.ts
@@ -1,6 +1,51 @@
 import { ChessBoard } from "./chess/Board";
 import { MapCor } from "./chess/Coordinates";
-let newGame = new ChessBoard();
-newGame.renderBoard();
-newGame.move(new MapCor(1, 3), new MapCor(2, 3));
-newGame.renderBoard();
+
+//chess algebraic notation to string
+let chessAGToCor = (ag: string) => {
+  if (!/[a-h][1-8]/.test(ag)) {
+    throw new Error("Enter valid move");
+  } else {
+    var yCor: number = 0;
+    var xCor = 8 - parseInt(ag[1]);
+    switch (ag[0]) {
+      case "a":
+        yCor = 0;
+      case "b":
+        yCor = 1;
+      case "c":
+        yCor = 2;
+      case "d":
+        yCor = 3;
+      case "e":
+        yCor = 4;
+      case "f":
+        yCor = 5;
+      case "g":
+        yCor = 6;
+      case "h":
+        yCor = 7;
+    }
+    console.log(yCor);
+    return new MapCor(xCor, yCor);
+  }
+};
+
+class GameController {
+  constructor() {
+    this.game = new ChessBoard();
+  }
+  private game: ChessBoard;
+  move(from: string, to: string): void {
+    this.game.move(chessAGToCor(from), chessAGToCor(to));
+  }
+  render() {
+    this.game.renderBoard();
+  }
+}
+
+// let game1 = new GameController();
+let game = new ChessBoard();
+game.renderBoard();
+// game1.move("a2", "a3");
+// game1.render();
